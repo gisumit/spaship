4da4942aa74f07d4d51844f7d63ede4bff86a3c0
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2022-04-07 18:14:48 +0530

    feat(property): Component for adding new property added.

---
+++ packages/manager/components/web-property/addProperty.tsx
@@ -0,0 +1,28 @@
+import { Card, EmptyState, EmptyStateVariant, Title } from "@patternfly/react-core";
+import styled from "styled-components";
+
+const AddPropertyBox = styled(Card)({
+  borderRadius: "8px",
+  opacity: 1,
+  height: "199px;",
+});
+
+const AddProperty = () => {
+  return (
+    <>
+      <Card isSelectable isRounded>
+        <AddPropertyBox>
+          <EmptyState variant={EmptyStateVariant.xs}>
+            <div className="spaship-circle spaship-plus">&#43;</div>
+            <br></br>
+            <Title headingLevel="h5" size="md">
+              New Web Property
+            </Title>
+          </EmptyState>
+        </AddPropertyBox>
+      </Card>
+    </>
+  );
+};
+
+export default AddProperty;
