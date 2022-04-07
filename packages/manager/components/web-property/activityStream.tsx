31d24686ecfb991f80bc88cb08d7157812e0874b
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2022-04-07 18:12:43 +0530

    feat(property): Activity stream component added.

---
+++ packages/manager/components/web-property/activityStream.tsx
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+import { Label, List, ListItem, Text, TextContent, TextVariants } from "@patternfly/react-core";
+import { ActivityProps, Properties } from "../models/props";
+
+const DivStyle = styled.div({
+  height: "590px",
+  width: "1017px",
+  border: "1px solid #D2D2D2;",
+  opacity: "1;",
+});
+
+const ActivityStream = ({ webprop }: Properties) => {
+  return (
+    <>
+      <TextContent>
+        <Text component={TextVariants.h1}>Activity Stream</Text>
+      </TextContent>
+      <br></br>
+      <DivStyle>
+        <div>
+          <br></br>
+          <List>
+            {webprop.map((activity: ActivityProps) => (
+              <ListItem key={activity.id}>
+                <TextContent>
+                  <Text component={TextVariants.small}>
+                    <Label color="green"> {activity.spaName}</Label> &nbsp; &nbsp; has been deployed over &nbsp;
+                    <Label color="green"> {activity.propertyName}</Label> on {activity.envs} at {activity.createdAt}
+                    <br />
+                  </Text>
+                </TextContent>
+              </ListItem>
+            ))}
+          </List>
+        </div>
+      </DivStyle>
+    </>
+  );
+};
+
+export default ActivityStream;
